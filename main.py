3efcdc86f12a6e6ca63b4f63cd102a613b899a16
olivialeahosb <olivia_osborne@biswashington.org>
2025-01-16 17:44:35 +0000

    here

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+with open("students.txt", "r") as students:
+    for line in students:
+        print(line.strip())
+ 
+with open("students.txt", "r") as students:
+    for line in students:
+        print(line[2])
+ 
+with open("students.txt", "r") as students:
+    for line in students:
+        if len(line) > 6:
+            print(line.strip())
+ 
+userinp = input("Insert your own name into file: ")
+ 
+with open("students.txt", "a") as students:
+    students.write(userinp + "\n")
+    print("Done")
+
 
